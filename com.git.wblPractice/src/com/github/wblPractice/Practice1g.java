3c18f1c1097255dd0dada8d249cf7ce0efbb8c2e
shashipx <shashi.489@gmail.com>
2015-08-21 10:19:37 -0700

    Loops

---
+++ com.git.wblPractice/src/com/github/wblPractice/Practice1g.java
@@ -0,0 +1,40 @@
+/*Write a program  that prints a pattern like below.
+ *
+ **
+ ***
+ ****
+ *****
+ ******		   
+ */
+package com.github.wblPractice;
+
+import java.util.Scanner;
+
+public class Practice1g {
+	public static void main(String[] args) {
+		System.out.println("Enter number of lines of pattern you want : ");
+		int n = new Scanner(System.in).nextInt();
+		// Practice1g pattern = new Practice1g();
+		// pattern.drawPattern(numberOfLines);
+		int vLn, hLn;
+		for (vLn = 1; vLn <= n; vLn++) {
+			for (hLn = 1; hLn <= vLn; hLn++) {
+				System.out.print("*");
+				// Can also print numbers using below line of code
+				// System.out.print(hLn+ " ");
+			}
+			System.out.println(" ");
+		}
+
+	}
+}
+/* Output on console
+Enter number of lines of pattern you want : 
+6
+* 
+** 
+*** 
+**** 
+***** 
+******    */
+
